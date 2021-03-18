a5174e1f2ddab4ea731721d691bb6a061b71e720
Eduardo Almeida <eduardoeandroid@gmail.com>
2021-03-17 21:25:31 -0300

    maquina de estados

---
+++ Assets/Script/State Machine/States/LoadState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadState : State {
+
+    public float porcentagem;
+    public override void Enter() {
+        base.Enter();
+        print("Agora vai");
+    }
+}
---
+++ Assets/Script/State Machine/States/RoamState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoamState : State {
+    public override void Enter() {
+        base.Enter();
+    }
+}
